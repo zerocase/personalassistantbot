bd3aff69aafdfa13b3f6bc325caf8a9593274274
zerocase <manysutuffyouneed@gmail.com>
2019-01-02 15:22:27 +0100

    Updating Sprites & Bug Fixing
    Updated the Sprite to fit my mechanics.
    Fixed certain bugs.
    Changed Collision Box for player.
    Improved  the camera2d.

--- Scenes/PlayerChar/PlayerChar.gd
+++ Scenes/PlayerChar/PlayerChar.gd
@@ -1,13 +1,17 @@
 extends KinematicBody2D
 export (int) var gravity = 900
 export (int) var jump_speed =-200
-export (int) var move_speed = 100
+export (int) var ACCELERATION = 100
 
 var velocity = Vector2()
 var jumping = false
 var jump_count = 2
 var pcubolds_collected = 0
 const MIN_JUMP_COUNT = 0
+const UP = Vector2(0,-1)
+func _ready():
+	$AnimationPlayer.play("Float")
+
 func get_input():
 	velocity.x=0
 	var right = Input.is_action_pressed('ui_right')
@@ -21,19 +25,17 @@ func get_input():
 		if is_on_floor() and not jump_press :
 			jump_count=2
 	if right:
-		velocity.x += move_speed
+		velocity.x += ACCELERATION
 	if left:
-		velocity.x -= move_speed
-	else:
-		_player_death()
+		velocity.x -= ACCELERATION
 
 func _physics_process(delta):
 	if get_owner()!= null:
 		pcubolds_collected = get_owner().cubolds_collected
-	$Label.text = str(jump_count)
+	get_node("Sprite/Label").text = str(jump_count)
 	get_input()
 	velocity.y += gravity*delta
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	velocity = move_and_slide(velocity,UP)
 
 func _player_death():
 	pass
