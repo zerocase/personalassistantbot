ddfbb287df9c0d0ffe57727dc8c46aefca2cc188
zerocase <manysutuffyouneed@gmail.com>
2019-01-07 22:23:13 +0100

    Level & Tileset
    Made a temporary tileset and tested a level with a couple of interesting concepts.

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -5,6 +5,11 @@ var cubolds_collected = 2
 
 func _ready():
 	pass
+	
+func _input(event):
+	var exit_key = Input.is_action_pressed("ui_cancel")
+	if exit_key:
+		get_tree().quit()
 
 func _process(delta):
 	get_node("PlayerChar/Sprite/Label").text = str(cubolds_collected)
